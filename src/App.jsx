7ee08ae7b67cd41be85af5d1cc285562eed059a6
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-04-02 21:53:39 -0400

    complete basic Route and Outlet setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home';
 
+const Shop = () => {
+  return (
+    <h1>I Am The Shop Page</h1>
+  )
+}
+
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/home' element={<Home />}>
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
