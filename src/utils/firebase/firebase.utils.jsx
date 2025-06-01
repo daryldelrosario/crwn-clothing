1c0e05968cc0c9f20b1669001b44a4df8b5512dc
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-06-01 14:21:23 -0400

    complete firebase config

---
+++ src/utils/firebase/firebase.utils.jsx
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC15S0ST8z74mF4jhw1TbTTswsOlHiyzPI",
+  authDomain: "crwn-clothing-db-94786.firebaseapp.com",
+  projectId: "crwn-clothing-db-94786",
+  storageBucket: "crwn-clothing-db-94786.firebasestorage.app",
+  messagingSenderId: "79084030940",
+  appId: "1:79084030940:web:f6a180b9b717beeb609d19"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
