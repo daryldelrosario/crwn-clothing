251d28aa50e3bf6e2fbe283dab7740471b8ce8bd
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-06-01 14:25:40 -0400

    complete google auth setup

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -13,3 +13,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
